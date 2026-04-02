345116111421811f69f625526b605ac0c52d570b
Harshit <harshitsb2615@gmail.com>
2026-04-02 15:45:01 +0530

    render deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const Database = require("better-sqlite3");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const DB_PATH = path.join(__dirname, "location-data.db");
+const DB_PATH = process.env.DB_PATH || path.join(__dirname, "location-data.db");
 
 const db = new Database(DB_PATH);
 db.pragma("journal_mode = WAL");
