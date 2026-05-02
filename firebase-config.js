6d5f47c9181a01ff7d102e7af74ed63ac9495884
Harshit <harshitsb2615@gmail.com>
2026-05-02 15:04:11 +0530

    Include firebase config

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBaFuw33zzZUme7CDUb7dpII3YeKgimN4w",
+  authDomain: "pingmereg.firebaseapp.com",
+  databaseURL: "https://pingmereg-default-rtdb.firebaseio.com",
+  projectId: "pingmereg",
+  storageBucket: "pingmereg.firebasestorage.app",
+  messagingSenderId: "1098521383686",
+  appId: "1:1098521383686:web:b2c777e80121311a144487"
+};
